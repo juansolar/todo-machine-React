f86d47ac0a38751ce51a36c0cf25dc4d488b39fc
juansolar <juansolarteo98@gmail.com>
2025-03-06 17:03:04 -0500

    Se actualizan los mensajes dependiento los Todos registrados.

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,9 +2,22 @@ import './TodoCounter.css';
 
 function TodoCounter({total, completed}){
     return(
-        <p className='counter-p'>
-            You have completed {completed} out of {total} tasks
-        </p>
+        <>
+            {total === 0 ? 
+                <p className='counter-p'>
+                    You have no tasks registered
+                </p>
+                : (total !== completed ? (
+                    <p className='counter-p'>
+                        You have completed {completed} out of {total} tasks
+                    </p>
+                ): (
+                    <p className='counter-p'>
+                        Congratulations, you completed all your tasks 🥳
+                    </p>
+                ))
+            }
+        </>
     );
 }
 
