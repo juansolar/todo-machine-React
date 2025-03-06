5f68603ecee8656ee83cf092cc9b11fa9b896ceb
juansolar <juansolarteo98@gmail.com>
2025-03-06 16:07:03 -0500

    Búsqueda y filtro de Todos

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const defaultTodos = [
   {title: 'tarea 3', description: 'Organizar la habitación tanto para el frio como el calor de esa ciudad', completed: false},
   {title: 'tarea 4', description: 'Organizar la casa de campo tanto para el frio como el calor de esa ciudad', completed: true},
   {title: 'tarea 5', description: 'Organizar la maleta tanto para el frio como el calor de esa ciudad', completed: true},
-  {title: 'viajar', description: 'Viajar a Popayán', completed: true},
+  {title: 'viajar', description: 'Viajar a Popayán', completed: true}
 ];
 
 const typeTodo = [
@@ -37,11 +37,27 @@ const description = {
 
 function App() {
 
-  const [todos, setTodos] = useState(defaultTodos);
-  const [searchValue, setSearchValue] = React.useState('');
+  const [todos, setTodos] = useState(defaultTodos); //Tareas
+  const [searchValue, setSearchValue] = React.useState('');//valor de búsqueda
+  const [filterValue, setFilterValue] = React.useState('all');//filtro de búsqueda
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
+
+  //Estado derivado
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const title = todo.title.toLocaleLowerCase();
+      const description = todo.description.toLocaleLowerCase();
+      const completed = todo.completed;
+      const value = searchValue.toLocaleLowerCase();
+
+      return ( (title.includes(value) || description.includes(value)) &&
+          (filterValue === 'all' ? todos : (filterValue === 'complete' ? completed : !completed))
+      );
+    }
+  );
+
 
   return (
     <div className='app'>
@@ -58,12 +74,12 @@ function App() {
         <div className='todo-container'>
           <div className='todo-search'>
             <TodoSearch setSearchValue={setSearchValue} searchValue={searchValue}/>
-            <TodoFilter typeTodo={typeTodo}/>
+            <TodoFilter typeTodo={typeTodo} setFilterValue={setFilterValue}/>
           </div>
           <TodoCounter completed={completedTodos} total={totalTodos} />
           <TodoList>
-            {defaultTodos.map( todo => (
-              <TodoItem key={todo.title} title={todo.title} description={todo.description} completed={todo.completed}/>) 
+            {searchedTodos.map( todo => (
+                <TodoItem key={todo.title} title={todo.title} description={todo.description} completed={todo.completed}/>)
               )}
           </TodoList>
         </div>
--- src/TodoFilter.js
+++ src/TodoFilter.js
@@ -1,10 +1,14 @@
 import './TodoFilter.css';
 
-function TodoFilter({typeTodo}){
+function TodoFilter({typeTodo, setFilterValue}){
+
     return(
-        <select className="SelectType">
+        <select className="SelectType" onChange={ (event) => setFilterValue(event.target.value) }>
             {typeTodo.map( (typeTodo, index) => (
-                <option className='SelectType-option' key={index} value={typeTodo.type}>{typeTodo.text}</option>
+                <option className='SelectType-option' 
+                    key={index} value={typeTodo.type} >
+                        {typeTodo.text}
+                </option>
             ) )}
         </select>
     );
